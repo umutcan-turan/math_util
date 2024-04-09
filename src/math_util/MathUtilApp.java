a6261a53fa870f1e6db5e75940931a5f425ab229
Umutcan Turan <mail@umutcan.org>
2024-04-09 17:01:53 +0300

    Implement isPrime

--- src/math_util/MathUtilApp.java
+++ src/math_util/MathUtilApp.java
@@ -6,3 +6,23 @@ class MathUtilApp {
 		
 	}
 }
+
+class Utils {
+	public static boolean isPrime(long number)
+	{
+		if (number < 2)
+			return false;
+		if (number % 2 == 0)
+			return number == 2;
+		if (number % 3 == 0)
+			return number == 3;
+		if (number % 5 == 0)
+			return number == 5;
+		if (number % 7 == 0)
+			return number == 7;
+		for (long i = 11; i * i < number; i += 2)
+			if (number % i == 0)
+				return false;
+		return true;
+	}
+}
